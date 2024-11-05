d02498834bc25e6f7ad2350dbf496867e5aee31b
nihithajamjuri <44652944+nihithajamjuri@users.noreply.github.com>
2024-11-05 19:04:53 +0530

    Create sys_info.py

---
+++ sys_info.py
@@ -0,0 +1,17 @@
+import socket
+import platform
+import os
+
+def get_sys_info():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    os_type = platform.system()
+    os_version = platform.version()
+
+    print(f"Hostname: {hostname}")
+    print(f"IP Address: {ip_address}")
+    print(f"Operating System: {os_type}")
+    print(f"OS Version: {os_version}")
+
+if __name__ == "__main__":
+    get_sys_info()
